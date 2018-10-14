17b54ef84ccb08bd1f477b9b56454b82ab99a1dc
mthomann-lilly <mitchell.thomann@lilly.com>
2018-10-14 10:18:29 -0400

    Finished working version of team generation data for testing

--- R/sfw.R
+++ R/sfw.R
@@ -14,11 +14,23 @@ add <- function(x, y) {
 
 #' Generate team data by week for testing
 #' 
-#' @param labels vector of team labels
+#' 
+#' @param team_labels vector of team labels
 #' @param weeks total number of weeks
-#' @param distribution list with elements, dist = c("norm"), mean, sd
+#' @param random_dist list with elements, dist = c("norm"), mean, sd
 #' @param decimal indicator for decimal scoring 
 #' 
-gen_team_dat <- function(labels, weeks, distribution, decimal) {
-  
+#' @import tidyverse
+#' 
+gen_team_dat <- function(team_labels=paste0("team",1:10),
+                         weeks=10, 
+                         random_dist=list(dist="norm",mean=100,sd=10), 
+                         decimal=F) {
+  if(random_dist$dist == "norm"){
+    team_dat <- dplyr::data_frame(team=rep(team_labels,weeks),
+                                  week=rep(1:weeks,each=length(team_labels)))
+    team_dat$scores <- rnorm(n=nrow(team_dat),mean=random_dist$mean,sd=random_dist$sd)
+    if(! decimal) floor(team_dat$scores)
+  }
+  return(team_dat)
 }
