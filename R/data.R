500cc16e60f3089c13af2b92fde44c6b06d0cff6
mitchell-thomann <mitchell.thomann@gmail.com>
2018-11-22 23:06:13 -0600

    Add fantasy dataset description

---
+++ R/data.R
@@ -0,0 +1,11 @@
+#' Fantasy Scores From 2018 fantasy league
+#'
+#' A dataset containing scores, week and team name from fantasy league
+#'
+#' @format A data_frame with 132 rows and 3 variables
+#' \describe{
+#'   \item{team}{team name}
+#'   \item{week}{week of season}
+#'   \item{scores}{team score}
+#' }
+"fantasy"
